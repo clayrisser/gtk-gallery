139d6ba6d28034b7f29f9e7a3a1a0a4f2bd837a8
Jam Risser <jam@codejam.ninja>
2019-08-16 01:11:32 -0700

    Improved organization

--- src/main.c
+++ src/main.c
@@ -9,17 +9,14 @@ int main(int argc, char* argv[]) {
   GtkWidget* window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
   gtk_window_set_title(GTK_WINDOW(window), "Gtk Gallery");
   gtk_widget_set_size_request(window, 200, 100);
-
   g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(destroy), NULL);
   g_signal_connect(G_OBJECT(window), "delete_event", G_CALLBACK(delete_event), NULL);
 
   GtkWidget* label = gtk_label_new("Gtk Gallery");
-
   gtk_container_add(GTK_CONTAINER(window), label);
+
   gtk_widget_show_all(window);
-
   gtk_main();
-
   return 0;
 }
 
